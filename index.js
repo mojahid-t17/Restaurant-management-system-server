3a1a0e9ba15a2e3b996da8b03e54624080392630
mojahid-t17 <mojahidult17@gmail.com>
2025-10-01 19:49:39 +0600

    deploy on vercel

--- index.js
+++ index.js
@@ -24,7 +24,7 @@ const client = new MongoClient(uri, {
 async function run() {
   try {
     // Connect the client to the server	(optional starting in v4.7)
-    await client.connect();
+    // await client.connect();
     const userCollection = client.db("restaurantDb").collection("users");
     const menuCollection = client.db("restaurantDb").collection("menu");
     const CartCollection = client.db("restaurantDb").collection("cart");
@@ -284,7 +284,7 @@ app.get("/menu-stats", verifyToken, verifyAdmin, async (req, res) => {
 
 
     // Send a ping to confirm a successful connection
-    await client.db("admin").command({ ping: 1 });
+    // await client.db("admin").command({ ping: 1 });
     console.log(
       "Pinged your deployment. You successfully connected to MongoDB!"
     );
